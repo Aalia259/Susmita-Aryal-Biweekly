63e4ce158fd90ed24acf84b8df707ee73106240a
aryalsusmit <aryalsusmita56@gmail.com>
2023-08-13 17:54:38 +1000

    initial code

---
+++ week 2 lecture/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_2_lecture
+{
+    class Program
+    
+        static void Main(string[] args)
+    { double mark1 = 35;
+      double mark2 = 36;
+      double mark3 = 57;
+      double mark4 = 99;
+    }
+        }
+    
+
